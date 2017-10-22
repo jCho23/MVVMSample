ec695ad85164c0e8f4616c18ef6c42335c81fc27
June Cho <junecho.ucb@gmail.com>
2017-10-22 09:12:11 -0700

    OnAppearing

--- MVVMSample/Views/PlaylistsPage.xaml.cs
+++ MVVMSample/Views/PlaylistsPage.xaml.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace MVVMSample.Views
 {
     public partial class PlaylistsPage : ContentPage
     {
+        private ObservableCollection<Playlist> _playlists = new ObservableColllection<Playlist> ();
+
         public PlaylistsPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing ()
+        {
+            playlistsListView.ItemsSource = _playlists;
+            base.OnAppearing(); 
+        }
+
+
+
+
+
+
+
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void OnPlaylistSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
