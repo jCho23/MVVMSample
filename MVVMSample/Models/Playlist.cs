92117a5d35caa24ad9f4068d97572ac1ce38fd39
June Cho <junecho.ucb@gmail.com>
2017-10-22 16:18:22 -0700

    cleanUp

--- MVVMSample/Models/Playlist.cs
+++ MVVMSample/Models/Playlist.cs
@@ -1,39 +1,37 @@
-﻿using System;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using MVVMSample.ViewModels;
-using Xamarin.Forms;
-
+﻿
 namespace MVVMSample.Models
 {
-    public class Playlist : BaseViewModel
+    ////This is a domain class
+    public class Playlist
     {
         ////No longer needed, since we added BaseViewModel
         //public event PropertyChangedEventHandler PropertyChanged;
 
         public string Title { get; set; }
 
-        private bool _isFavorite;
-        public bool IsFavorite
-        {
-            get { return _isFavorite; }
-            set
-            {
-                SetValue(ref _isFavorite, value);
-                ////Moved to BaseViewModel
-                //if (_isFavorite == value)
-                //    return;
+        public bool IsFavorite { get; set; }
 
-                //_isFavorite = value;
-                //OnPropertyChanged();
-                OnPropertyChanged(nameof(Color));
-            }
-        }
+        ////Moved to PlaylistViewModel
+        ////Everything related to the UI is in the PlaylistViewModel  
+        //{
+        //    get { return _isFavorite; }
+        //    set
+        //    {
+        //        SetValue(ref _isFavorite, value);
+        //        ////Moved to BaseViewModel
+        //        //if (_isFavorite == value)
+        //        //    return;
 
-        public Color Color
-        {
-            get { return IsFavorite ? Color.HotPink : Color.Black; }
-        }
+        //        //_isFavorite = value;
+        //        //OnPropertyChanged();
+        //        OnPropertyChanged(nameof(Color));
+        //    }
+        //}
+
+        //public Color Color
+        //{
+        //    get { return IsFavorite ? Color.HotPink : Color.Black; }
+        //}
 
         ////Moved to BaseViewModel
         //private void OnPropertyChanged([CallerMemberName] string propertyName = null)
