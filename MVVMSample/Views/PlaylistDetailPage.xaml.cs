08e2f07222683aa13af35ce8ce6b936834b2e507
June Cho <junecho.ucb@gmail.com>
2017-10-22 08:54:28 -0700

    Added PlaylistDetailPage

---
+++ MVVMSample/Views/PlaylistDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MVVMSample.Views
+{
+    public partial class PlaylistDetailPage : ContentPage
+    {
+        public PlaylistDetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
