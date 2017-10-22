437398eb9f1f05aa40fb9eb073123ac46ad09f02
June Cho <junecho.ucb@gmail.com>
2017-10-22 11:39:53 -0700

    SetValue Method added

--- MVVMSample/ViewModels/BaseViewModel.cs
+++ MVVMSample/ViewModels/BaseViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
@@ -11,6 +12,17 @@ namespace MVVMSample.ViewModels
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetValue<T>(ref T backingField, T value)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingField, value))
+                return;
+
+            backingField == value;
+
+            OnPropertyChanged();
+
         }
     }
 }
