f272f4a4307f36ca3f8389242075ee2f0cf969c5
June Cho <junecho.ucb@gmail.com>
2017-10-24 20:09:04 -0700

    Cleanup

--- MVVMSample/Views/PlaylistsPage.xaml.cs
+++ MVVMSample/Views/PlaylistsPage.xaml.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-
-using Xamarin.Forms;
-using MVVMSample.Models;
-using MVVMSample.ViewModels;
+﻿using Xamarin.Forms;
 using MVVMSample.Services;
+using MVVMSample.ViewModels;
 
 namespace MVVMSample.Views
 {
@@ -23,26 +18,9 @@ namespace MVVMSample.Views
             base.OnAppearing(); 
         }
 
-        ////Implemented ICommand, no longer need event handler
-        //void OnAddPlaylist(object sender, System.EventArgs e)
-        //{
-            //(BindingContext as PlaylistsViewModel).AddPlaylist();
-            ////Moved to ViewModel
-            //var newPlaylist = "Playlist" + (_playlists.Count + 1);
-
-            //_playlists.Add(new Playlist {Title = newPlaylist})
-
         void OnPlaylistSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
         {
             ViewModel.SelectPlaylistCommand.Execute(e.SelectedItem);
-            ////Moved to ViewModel
-            //if (e.SelectedItem == null)
-            //    return;
-
-            //var playlist = e.SelectedItem as Playlist;
-            //playlist.IsFavorite = !playlist.IsFavorite;
-
-            //playlistsListView.SelectedItem = null;
         }
 
         private PlaylistsViewModel ViewModel
