7bac92367c8428a5c68b2c9c193097bd84fd0770
June Cho <junecho.ucb@gmail.com>
2017-10-25 17:47:55 -0700

    CleanUp Models

--- MVVMSample/Models/Playlist.cs
+++ MVVMSample/Models/Playlist.cs
@@ -1,11 +1,7 @@
 ﻿namespace MVVMSample.Models
 {
-    ////This is a domain class
     public class Playlist
     {
-        //No longer needed, since we added BaseViewModel
-        //public event PropertyChangedEventHandler PropertyChanged;
-
         public string Title { get; set; }
 
         public bool IsFavorite { get; set; }
