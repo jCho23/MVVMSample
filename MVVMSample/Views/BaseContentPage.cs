97a6ca7c0debec067f10b0427b38c5ad237ee1d5
June Cho <junecho.ucb@gmail.com>
2017-10-26 14:49:35 -0700

    added BaseContentPage

--- MVVMSample/Views/BaseContentPage.cs
+++ MVVMSample/Views/BaseContentPage.cs
@@ -1,10 +1,59 @@
 ﻿using System;
+using System.Threading.Tasks;
+using MVVMSample.ViewModels;
+using Xamarin.Forms;
+
 namespace MVVMSample.Views
 {
-    public class BaseContentPage
+    public abstract class BaseContentPage<T> : ContentPage where T : BaseViewModel, new()
     {
-        public BaseContentPage()
+        T viewModel;
+
+        protected BaseContentPage()
         {
+            this.ViewModel = Activator.CreateInstance<T>();
+        }
+
+        /// <summary>
+        /// Gets or sets the view model.
+        /// </summary>
+        /// <value>The view model.</value>
+        public T ViewModel
+        {
+            get { return this.viewModel; }
+            set
+            {
+                viewModel = value;
+                BindingContext = viewModel;
+                this.SetBinding(TitleProperty, nameof(ViewModel.Title));
+ 
+            }
+        }
+
+        //public abstract void SubscribeEventHandlers();
+        public virtual void SubscribeEventHandlers()
+        {
+            
+        }
+
+        //public abstract void UnsubscribeEventHandlers();
+        public virtual void UnsubscribeEventHandlers()
+        {
+
+        }
+
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            SubscribeEventHandlers();
+        }
+
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            UnsubscribeEventHandlers();
         }
     }
 }
